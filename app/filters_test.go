90118c2b450f2666f055952dc1cc7ad8714ef30e
divyendu <divyendua@gmail.com>
2025-01-23 00:49:49 -0500

    refactor code

---
+++ app/filters_test.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"math"
+	"testing"
+)
+
+func TestBlur(t *testing.T) {
+	img, _ := NewImage(1, 1)
+	img.p[0][0] = Pixel{r: 0.4, g: 0.2, b: 0.3, a: 1}
+
+	img, _ = img.Blur()
+
+	if math.Abs(img.p[0][0].r-0.1) >= 0.001 || math.Abs(img.p[0][0].g-0.05) >= 0.001 || math.Abs(img.p[0][0].b-0.075) >= 0.001 {
+		t.Error("GetGrayScaleByIntensity function failed")
+	}
+}
+
+func TestSharpen(t *testing.T) {
+	img, _ := NewImage(1, 1)
+	img.p[0][0] = Pixel{r: 0.4, g: 0.2, b: 0.3, a: 1}
+
+	img, _ = img.Sharpen()
+
+	if math.Abs(img.p[0][0].r-0.4) >= 0.001 || math.Abs(img.p[0][0].g-0.2) >= 0.001 || math.Abs(img.p[0][0].b-0.3) >= 0.001 {
+		t.Error("GetGrayScaleByIntensity function failed")
+	}
+}
+
+func TestEdgeDetection(t *testing.T) {
+	img, _ := NewImage(1, 1)
+	img.p[0][0] = Pixel{r: 0.4, g: 0.2, b: 0.3, a: 1}
+
+	img, _ = img.EdgeDetect()
+
+	if img.p[0][0].r != 1 || img.p[0][0].g != 1 || img.p[0][0].b != 1 {
+		t.Error("EdgeDetect function failed")
+	}
+}
--- app/img.go
+++ app/img.go
@@ -117,35 +117,6 @@ func (img *Img) GetGrayScaleByIntensity() (*Img, error) {
 	)
 }
 
-func (img *Img) Blur() (*Img, error) {
-	blurFilter := [][]float64{
-		{1.0 / 16, 1.0 / 8, 1.0 / 16},
-		{1.0 / 8, 1.0 / 4, 1.0 / 8},
-		{1.0 / 16, 1.0 / 8, 1.0 / 16},
-	}
-	return applyFilter(img, &blurFilter)
-}
-
-func (img *Img) Sharpen() (*Img, error) {
-	sharpenFilter := [][]float64{
-		{-1.0 / 8, -1.0 / 8, -1.0 / 8, -1.0 / 8, -1.0 / 8},
-		{-1.0 / 8, 1.0 / 4, 1.0 / 4, 1.0 / 4, -1.0 / 8},
-		{-1.0 / 8, 1.0 / 4, 1.0, 1.0 / 4, -1.0 / 8},
-		{-1.0 / 8, 1.0 / 4, 1.0 / 4, 1.0 / 4, -1.0 / 8},
-		{-1.0 / 8, -1.0 / 8, -1.0 / 8, -1.0 / 8, -1.0 / 8},
-	}
-	return applyFilter(img, &sharpenFilter)
-}
-
-func (img *Img) EdgeDetect() (*Img, error) {
-	edgeFilter := [][]float64{
-		{-1.0, -1.0, -1.0},
-		{-1.0, 8.0, -1.0},
-		{-1.0, -1.0, -1.0},
-	}
-	return applyFilter(img, &edgeFilter)
-}
-
 func (img *Img) HaarCompress(ratio float64) (*Img, error) {
 	if ratio < 0 || ratio > 1 {
 		return nil, errors.New("invalid compression ratio")
@@ -178,29 +149,4 @@ func process(f func(int, int, *Img), img *Img) (*Img, error) {
 		}
 	}
 	return newImg, nil
-}
-
-func applyFilter(img *Img, filterPointer *[][]float64) (*Img, error) {
-	filter := *filterPointer
-	return process(
-		func(h, w int, newImg *Img) {
-			steps := len(filter) / 2
-			r := 0.
-			g := 0.
-			b := 0.
-			for i := -steps; i <= steps; i++ {
-				for j := -steps; j <= steps; j++ {
-					if h+i >= 0 && h+i < img.h && w+j >= 0 && w+j < img.w {
-						r += filter[i+steps][j+steps] * img.p[h+i][w+j].r
-						g += filter[i+steps][j+steps] * img.p[h+i][w+j].g
-						b += filter[i+steps][j+steps] * img.p[h+i][w+j].b
-					}
-				}
-			}
-			newImg.p[h][w].r = clampPixelValue(r)
-			newImg.p[h][w].g = clampPixelValue(g)
-			newImg.p[h][w].b = clampPixelValue(b)
-			newImg.p[h][w].a = img.p[h][w].a
-		}, img,
-	)
 }
