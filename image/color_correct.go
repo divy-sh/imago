b9a4f174f7f0df5149d966ff66637fd61d79e363
divyendu <divyendua@gmail.com>
2025-01-26 02:28:32 -0500

    use average for color correct instead of peak value

--- image/color_correct.go
+++ image/color_correct.go
@@ -7,26 +7,29 @@ func colorCorrect(img *Img) (*Img, error) {
 	histSize := 256
 	redP, greenP, blueP := getHistogramData(img, histSize)
 	red, green, blue := *redP, *greenP, *blueP
-	rPeak, gPeak, bPeak := getMaxHistColorValues(&red, &green, &blue, histSize)
-	average := (rPeak + gPeak + bPeak) / 3
+	rAvg, gAvg, bAvg := getAvgHistColorValues(&red, &green, &blue, histSize)
+	average := (rAvg + gAvg + bAvg) / 3
+	rShift := rAvg - average
+	gShift := gAvg - average
+	bShift := bAvg - average
+	fmt.Println(rShift, gShift, bShift)
 	return process(
 		func(i, j int, newImg *Img) {
-			newImg.p[i][j].r = clampPixelValue(img.p[i][j].r + (float64(average-rPeak))/float64(histSize))
-			newImg.p[i][j].g = clampPixelValue(img.p[i][j].g + (float64(average-gPeak))/float64(histSize))
-			newImg.p[i][j].b = clampPixelValue(img.p[i][j].b + (float64(average-bPeak))/float64(histSize))
-			fmt.Println(newImg.p[i][j])
+			newImg.p[i][j].r = clampPixelValue(img.p[i][j].r + rShift)
+			newImg.p[i][j].g = clampPixelValue(img.p[i][j].g + gShift)
+			newImg.p[i][j].b = clampPixelValue(img.p[i][j].b + bShift)
 			newImg.p[i][j].a = img.p[i][j].a
 		}, img,
 	)
 }
 
-func getMaxHistColorValues(red, green, blue *[]int, histSize int) (int, int, int) {
-	rMax, gMax, bMax := 0, 0, 0
+func getAvgHistColorValues(red, green, blue *[]int, histSize int) (float64, float64, float64) {
+	rAvg, gAvg, bAvg := 0, 0, 0
 
-	for i := 50; i < histSize-50; i++ {
-		rMax = max(rMax, (*red)[i])
-		gMax = max(gMax, (*green)[i])
-		bMax = max(bMax, (*blue)[i])
+	for i := 0; i < histSize; i++ {
+		rAvg += (*red)[i]
+		gAvg += (*green)[i]
+		bAvg += (*blue)[i]
 	}
-	return rMax, gMax, bMax
+	return float64(rAvg) / float64(histSize), float64(gAvg) / float64(histSize), float64(bAvg) / float64(histSize)
 }
